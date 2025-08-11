b738852e8805d7c909b81363339c6c05b3f59298
Florian <florian.lahitte@uvltd.tech>
2025-08-11 16:26:27 -0400

    postgre user

---
+++ migrations/20250811195409_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO "public"."users"
+("id", "email", "first_name", "last_name", "password", "access_level", "created_at", "updated_at")
+VALUES(1, 'admin@admin.com', 'bob', 'lahitte', '$2a$12$oBU9WLTWor.YVNR9w/lU5udakick4mNCxnbWJHU8LFVyxQha9a/bO
+', 3, '2025-08-05 00:00:00.000', '2025-08-05 00:00:00.000');
